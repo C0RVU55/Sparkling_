92afd7e60ffa5a9b7a080d17ad84f1f4e00194fc
'werg147' <'werg147@naver.com'>
2021-03-15 12:28:55 +0900

    브랜치테스트

---
+++ src/main/java/com/javaex/controller/BranchTest.java
@@ -0,0 +1,12 @@
+package com.javaex.controller;
+
+public class BranchTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("브랜치 테스트");
+
+	}
+
+}
